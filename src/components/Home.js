06852aa2352f123fcd82ada24ed38e099274e82a
Manish Pritmani <manish.pritmani06@gmail.com>
2022-04-10 02:03:07 +0530

    Added Home

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+        <div className="card mx-5 my-5">
+        <div className="card-header">
+          Welcome to Innovaccer Reward Program
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">Benefits of Store</h5>
+          <p className="card-text">1. You choose your happy gifts and rewards.</p>
+          <p className="card-text">2. We know how it feels for same gifts all the time.</p>
+          <p className="card-text">3. We try as to give you feeling of excitement when you receive the rewards.</p>
+         
+      </div>
+    </div>
+  );
+};
+
+export default Home;
